268ef4b54ccf9350c2d179d31e5683eee8453153
de-evg <d.e.min@yandex.ru>
2021-02-17 12:23:40 +0300

    remove composeWithDevTools for deploy on heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./components/app/app";
 import root from "./store/reducers/root.js";
 import Api from "./api/api";
@@ -14,7 +14,7 @@ const api = new Api(BACKEND_URL);
 
 const store = createStore(
   root,
-  composeWithDevTools(applyMiddleware(thunk.withExtraArgument(api)))
+  applyMiddleware(thunk.withExtraArgument(api))
 );
 
 render(
